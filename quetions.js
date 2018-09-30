d822d10c116826bc53af969b87c842fe235c797e
shefy123 <38942966+shefy123@users.noreply.github.com>
2018-09-30 22:32:38 +0500

    Add files via upload

---
+++ quetions.js
@@ -0,0 +1,42 @@
+
+let quetions = [{
+    "quetion" : "The day before the day before yesterday is three days after Saturday. What day is it today?",
+    "option1" : "Monday",
+    "option2" : "Tuesday",    
+    "option3" : "Thursday",
+    "option4" : "Friday",
+    "answer" :  "Friday"
+},{
+    "quetion" : "Library is to book as book is to?",
+    "option1" : "Binding",
+    "option2" : "Copy",    
+    "option3" : "Page",
+    "option4" : "Cover",
+    "answer" :  "Page."
+},
+{
+    "quetion" : "Which of the following is not a universal artistic theme?",
+    "option1" : "sporting events",
+    "option2" : "nature",    
+    "option3" : "portraiture",
+    "option4" : " social commentary",
+    "answer" :  "Sporting events is a predominantly Western artistic theme."
+},
+{
+    "quetion" : " Find the next number in the following series: 15 12 13 10 11 8",
+    "option1" : "3",
+    "option2" : "9",    
+    "option3" : "12",
+    "option4" : "14",
+    "answer" :  "First in the series deduct 3, then add one."
+},
+{
+    "quetion" : "A baguette is to be cut into 10 equal pieces. How many cuts does the baker need to make?",
+    "option1" : "8",
+    "option2" : "9",    
+    "option3" : "10",
+    "option4" : "11",
+    "answer" :  "9"
+},
+    
+]
---
+++ script.js
@@ -0,0 +1,51 @@
+let currentQuetion = 0;
+let score = 0 ;
+let totQuetions = quetions.length;
+
+let container = document.getElementById('quiz-container');
+let quetionEL = document.getElementById('question');
+let opt1 = document.getElementById('opt1');
+let opt2 = document.getElementById('opt2');
+let opt3 = document.getElementById('opt3');
+let opt4 = document.getElementById('opt4');
+let nextButton = document.getElementById('nextButton');
+let resultCont = document.getElementById('result');
+
+function loadQuestion (questionIndex) {
+     var q = quetions[questionIndex];
+     quetionEL.textContent = (questionIndex + 1) + '. ' + q.quetion;
+     opt1.textContent = q.option1;
+     opt2.textContent = q.option2;
+     opt3.textContent = q.option3;
+     opt4.textContent = q.option4;
+     
+};
+
+function loadNextQuestion () {
+    var selectedOption =  document.querySelector('input[type=radio]:checked'),
+        
+    if ( !selectedOption ) {
+        alert('please select your answer!');
+        console.log('sdhjkasd');
+        return;
+    }
+    // when answer is true its add 10 on score....
+    var answer = selectedOption.value;
+    if (quetions[currentQuetion].answer == answer) {
+        score += 10;
+    }
+
+    selectedOption.checked = false;
+    currentQuetion++;
+    if (currentQuetion == totQuetions -1) {
+        nextButton.textContent = 'finish';
+    } 
+    if (currentQuetion == totQuetions) {
+        container.style.display =  'none';
+        resultCont.style.display = '';
+        resultCont.textContent = 'your score: ' + score;
+        return;
+    }
+    loadQuestion(currentQuetion);
+}
+
